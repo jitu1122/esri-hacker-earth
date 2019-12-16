32e469144e37e2e750e5fced9b138b8f1693068b
Jitu <jitendra@pdfminion.com>
2019-12-17 02:13:46 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {AppService} from './app.service';
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
+
+export interface PeriodicElement {
+  balance: string;
+  transDate: string;
+  depositAMT: any;
+  transDetails: string;
+  withdrawalAMT: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'esri-challange';
+export class AppComponent implements OnInit {
+  displayedColumns: string[] = ['transDate', 'transDetails', 'depositAMT', 'withdrawalAMT', 'balance'];
+  selectedAccount = '';
+  showTable = false;
+
+  dataSource: any;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+
+
+  constructor(public appService: AppService) {
+  }
+
+
+  ngOnInit(): void {
+    this.appService.getTransactions().subscribe(() => {
+      console.log(this.appService.entries);
+    });
+
+  }
+
+  onSelect() {
+    this.showTable = true;
+    console.log(this.selectedAccount);
+    this.dataSource = new MatTableDataSource<PeriodicElement>(this.appService.entries[this.selectedAccount]);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+
+  }
+
+  objKeys(obj) {
+    return Object.keys(obj);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatPaginatorModule,
+  MatSelectModule,
+  MatSortModule,
+  MatTableModule
+} from '@angular/material';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
